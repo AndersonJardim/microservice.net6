065aeb6128f478e108ace9b0d3082de747683234
Anderson Jardim <andersonjardim@gmail.com>
2023-02-25 01:43:16 -0300

    public class ProductRepository (interface parte 3)

--- GeekShopping.ProductAPI/Repository/ProductRepository.cs
+++ GeekShopping.ProductAPI/Repository/ProductRepository.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Model;
 using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
 using System.Runtime.CompilerServices;
 
 namespace GeekShopping.ProductAPI.Repository
@@ -15,28 +17,31 @@ namespace GeekShopping.ProductAPI.Repository
             _context = context;
             _mapper = mapper;
         }
+        public async Task<IEnumerable<ProductVO>> FindAll()
+        {
+            List<Product> products = await _context.Products.ToListAsync();
+            return _mapper.Map<List<ProductVO>>(products);
+        }
+
+        public async Task<ProductVO> FindById(long id)
+        {
+            //Product product = await _context.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
+            Product product = await _context.Products.Where(p => p.Id == id)
+                .FirstOrDefaultAsync();
+            return _mapper.Map<ProductVO>(product);
+        }
 
         public Task<ProductVO> Create(ProductVO vo)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> Delete(long id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<ProductVO>> FindAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ProductVO> FindById(long id)
-        {
-            throw new NotImplementedException();
-        }
-
         public Task<ProductVO> Update(ProductVO vo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(long id)
         {
             throw new NotImplementedException();
         }
